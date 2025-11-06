261cc7f32bdf219faa2ec9a5e13eefdaab084fc1
SamuelGombar <samogombar@gmail.com>
2025-11-06 15:46:11 +0100

    real robot odometry

---
+++ sim.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd /opt/kobuki/sim && ./kobukiSIM
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/usr/bin/gnome-terminal --tab -- bash -c "ros2 run uds_kobuki_ros uds_kobuki_ros; exec bash" &
+/usr/bin/gnome-terminal --tab -- bash -c "rviz2; exec bash" &
+/usr/bin/gnome-terminal --tab -- bash -c "ros2 run ros2_laser_scan_matcher laser_scan_matcher; exec bash" &
+/usr/bin/gnome-terminal --tab -- bash -c "ros2 run robot_control_cpp teleop; exec bash" &
