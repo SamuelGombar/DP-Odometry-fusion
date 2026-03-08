4ee048c918c7cb0f0f0ed83d2423742d13309f63
SamuelGombar <samogombar@gmail.com>
2026-03-08 16:18:33 +0100

    debug clouds fixed frame

--- src/genz-icp/ros/ros2/OdometryServer.cpp
+++ src/genz-icp/ros/ros2/OdometryServer.cpp
@@ -257,21 +257,21 @@ void OdometryServer::PublishClouds(const rclcpp::Time &stamp,
 
     if (!publish_odom_tf_) {
         // debugging happens in an egocentric world
-        std_msgs::msg::Header cloud_header;
-        cloud_header.stamp = stamp;
-        cloud_header.frame_id = cloud_frame_id;
+        // std_msgs::msg::Header cloud_header;
+        // cloud_header.stamp = stamp;
+        // cloud_header.frame_id = cloud_frame_id;
 
-        map_publisher_->publish(std::move(EigenToPointCloud2(genz_map, odom_header)));
-        planar_points_publisher_->publish(std::move(EigenToPointCloud2(planar_points, cloud_header)));
-        non_planar_points_publisher_->publish(std::move(EigenToPointCloud2(non_planar_points, cloud_header)));
+        // map_publisher_->publish(std::move(EigenToPointCloud2(genz_map, odom_header)));
+        // planar_points_publisher_->publish(std::move(EigenToPointCloud2(planar_points, cloud_header)));
+        // non_planar_points_publisher_->publish(std::move(EigenToPointCloud2(non_planar_points, cloud_header)));
 
         // // std_msgs::msg::Header cloud_header;
         // // cloud_header.stamp = stamp;
         // // cloud_header.frame_id = cloud_frame_id;
 
-        // map_publisher_->publish(std::move(EigenToPointCloud2(genz_map, odom_header)));
-        // planar_points_publisher_->publish(std::move(EigenToPointCloud2(planar_points, odom_header)));
-        // non_planar_points_publisher_->publish(std::move(EigenToPointCloud2(non_planar_points, odom_header)));
+        map_publisher_->publish(std::move(EigenToPointCloud2(genz_map, odom_header)));
+        planar_points_publisher_->publish(std::move(EigenToPointCloud2(planar_points, odom_header)));
+        non_planar_points_publisher_->publish(std::move(EigenToPointCloud2(non_planar_points, odom_header)));
 
         return;
     }
