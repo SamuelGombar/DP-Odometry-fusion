61b99117567035ee0f48b397931ab3f96251c23a
SamuelGombar <samogombar@gmail.com>
2026-03-19 11:54:05 +0100

    recording of runs

--- start_scripts/benchmark_bag.sh
+++ start_scripts/benchmark_bag.sh
@@ -1,5 +1,5 @@
 /usr/bin/gnome-terminal --tab -- bash -c "rviz2 -d /home/samuelg9/ros2_ws_host/rviz/genz_wheel_ekf_fusion_benchmark.rviz; exec bash" &
-/usr/bin/gnome-terminal --tab -- bash -c "ros2 bag play /home/samuelg9/ros2_ws_host/recordings/candy_7m --topics /scan_merged /scan_merged_filtered /amrapi/sensor/velocity /hw_layer/imu/sensor/data; exec bash" &
+/usr/bin/gnome-terminal --tab -- bash -c "ros2 bag play /home/samuelg9/ros2_ws_host/recordings/bp/candy_7m --topics /scan_merged /scan_merged_filtered /amrapi/sensor/velocity /hw_layer/imu/sensor/data; exec bash" &
 /usr/bin/gnome-terminal --tab -- bash -c "ros2 run robot_control_cpp wheel_odom_publisher; exec bash" &
 /usr/bin/gnome-terminal --tab -- bash -c "ros2 run robot_control_cpp scan_republisher; exec bash" &
 /usr/bin/gnome-terminal --tab -- bash -c "ros2 run robot_control_cpp scan_to_pc; exec bash" &
@@ -13,5 +13,5 @@
 sleep 4
 /usr/bin/gnome-terminal --tab -- bash -c "ros2 run robot_control_cpp gt_wrapper; exec bash" &
 sleep 2
-/usr/bin/gnome-terminal --tab -- bash -c "ros2 bag play /home/samuelg9/ros2_ws_host/recordings/ground_truth_topic_candy_7m --rate 0.1; exec bash" &
+/usr/bin/gnome-terminal --tab -- bash -c "ros2 bag play /home/samuelg9/ros2_ws_host/recordings/gt/ground_truth_topic_candy_7m --rate 0.1; exec bash" &
 /usr/bin/gnome-terminal --tab -- bash -c "ros2 run robot_control_cpp odom_to_path --ros-args -p odometry_topic:=/ground_truth_wrapper -p path_topic:=/ground_truth_path; exec bash" &
--- start_scripts/record_gt.sh
+++ start_scripts/record_gt.sh
@@ -1,20 +1,36 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Start recorder in background
-ros2 bag record --topics /ground_truth -o /home/samuelg9/ros2_ws_host/recordings/ground_truth_topic_candy_7m &
+# # Start recorder in background
+# ros2 bag record --topics /ground_truth -o /home/samuelg9/ros2_ws_host/recordings/ground_truth_topic_candy_7m &
+# RECORD_PID=$!
+
+# # Start republisher in background
+# ros2 run robot_control_cpp groundtruth_republisher &
+# REPRUB_PID=$!
+
+# sleep 3
+# # Play bag — blocks until the bag finishes
+# ros2 bag play /home/samuelg9/ros2_ws_host/recordings/candy_7m --exclude-topics "/hw_layer/addon_mission_state"
+
+# # Kill recorder and republisher once play is done
+# kill $RECORD_PID $REPRUB_PID
+# wait $RECORD_PID $REPRUB_PID 2>/dev/null
+
+# paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+
+
+ros2 bag record --topics /ground_truth -o /home/samuelg9/ros2_ws_host/recordings/gt/ground_truth_topic_candy_7m &
 RECORD_PID=$!
 
-# Start republisher in background
 ros2 run robot_control_cpp groundtruth_republisher &
 REPRUB_PID=$!
 
 sleep 3
-# Play bag — blocks until the bag finishes
-ros2 bag play /home/samuelg9/ros2_ws_host/recordings/candy_7m --exclude-topics "/hw_layer/addon_mission_state"
+ros2 bag play /home/samuelg9/ros2_ws_host/recordings/bp/candy_7m --exclude-topics "/hw_layer/addon_mission_state"
 
-# Kill recorder and republisher once play is done
 kill $RECORD_PID $REPRUB_PID
 wait $RECORD_PID $REPRUB_PID 2>/dev/null
+paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 
 
 # /usr/bin/gnome-terminal --tab -- bash -c "ros2 bag record --topics /ground_truth -o /home/samuelg9/ros2_ws_host/recordings/ground_truth_topic_candy_7m; exec bash" &
