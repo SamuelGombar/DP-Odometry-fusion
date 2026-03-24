f81e879dcb87f0f2bbdfba57aa82577122f5de15
SamuelGombar <samogombar@gmail.com>
2026-03-24 14:23:59 +0100

    .sh files, after compression of output bags

---
+++ recordings/output/play_compressed.sh
@@ -0,0 +1,9 @@
+BAG_NAME="best/genz_ekf_Ralf_7m_compressed"
+
+/usr/bin/gnome-terminal --tab -- bash -c "rviz2 -d /home/samuelg9/ros2_ws_host/rviz/genz_wheel_ekf_fusion_benchmark.rviz; exec bash" &
+
+/usr/bin/gnome-terminal --tab -- bash -c "ros2 bag play /home/samuelg9/ros2_ws_host/recordings/output/${BAG_NAME}; exec bash" &
+/usr/bin/gnome-terminal --tab -- bash -c "ros2 run robot_control_cpp odom_to_path --ros-args -p odometry_topic:=/wheel_odom -p path_topic:=/wheel_odom_path; exec bash" &
+/usr/bin/gnome-terminal --tab -- bash -c "ros2 run robot_control_cpp odom_to_path --ros-args -p odometry_topic:=/ground_truth_wrapper -p path_topic:=/ground_truth_path; exec bash" &
+/usr/bin/gnome-terminal --tab -- bash -c "ros2 run robot_control_cpp odom_to_path --ros-args -p odometry_topic:=/genz/odometry -p path_topic:=/genz/trajectory; exec bash" &
+/usr/bin/gnome-terminal --tab -- bash -c "ros2 run robot_control_cpp odom_to_path --ros-args -p odometry_topic:=/odometry/filtered -p path_topic:=/odometry/path; exec bash" &
--- start_scripts/benchmark_bag_ekf.sh
+++ start_scripts/benchmark_bag_ekf.sh
@@ -1,5 +1,5 @@
 BAG_NAME="Ralf_7m"
-RECORD=true
+RECORD=false
 
 /usr/bin/gnome-terminal --tab -- bash -c "rviz2 -d /home/samuelg9/ros2_ws_host/rviz/genz_wheel_ekf_fusion_benchmark.rviz; exec bash" &
 /usr/bin/gnome-terminal --tab -- bash -c "ros2 bag play /home/samuelg9/ros2_ws_host/recordings/bp/${BAG_NAME} --topics /scan_merged /amrapi/sensor/velocity /hw_layer/imu/sensor/data; exec bash" &
@@ -15,7 +15,7 @@ RECORD=true
 
 if [ "$RECORD" = true ]; then
   rm -rf /home/samuelg9/ros2_ws_host/recordings/output/genz_ekf_${BAG_NAME}
-  /usr/bin/gnome-terminal --tab -- bash -c "ros2 bag record -o /home/samuelg9/ros2_ws_host/recordings/output/genz_ekf_${BAG_NAME} /genz/local_map /genz/non_planar_points /genz/odometry /genz/planar_points /genz/trajectory /ground_truth /ground_truth_path /ground_truth_wrapper /imu /odometry/filtered /odometry/path /pointcloud_topic /scan_merged /scan_merged_c /tf /tf_static /wheel_odom /wheel_odom_path; exec bash" &
+  /usr/bin/gnome-terminal --tab -- bash -c "ros2 bag record -o /home/samuelg9/ros2_ws_host/recordings/output/genz_ekf_${BAG_NAME} /genz/local_map /genz/non_planar_points /genz/odometry /genz/planar_points /ground_truth_wrapper /imu /odometry/filtered /scan_merged_c /tf /tf_static /wheel_odom; exec bash" &
 fi
 
 sleep 4
